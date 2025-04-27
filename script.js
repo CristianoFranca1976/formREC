103eab17b4d61729376b455a6035df8a236990f2
CrstianoFranca1976 <cristiano.franca@hotmail.co.uk>
2025-04-27 20:24:09 +0100

    third commit

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 /* ---- Elementos ---- */
 
-const inputNome = document.querySelector('input#nome')
-const inputTelefone = document.querySelector('input#telefone')
-const textareaMensagem = document.querySelector('textarea#mensagem')
-const buttonEnviar = document.querySelector('button#enviar')
+const inputNome = document.querySelector('input#nome');
+const inputTelefone = document.querySelector('input#telefone');
+const textareaMensagem = document.querySelector('textarea#mensagem');
+const buttonEnviar = document.querySelector('button#enviar');
+const recrutadorNome = document.querySelector('input#recrutador');
+const recrutadorId = document.querySelector('input#recrutador-id');
+const link = "https://form-rec-cristiano-f.vercel.app/";
 
 /* ---- Eventos ---- */
 
@@ -15,11 +18,13 @@ function enviarFormulario () {
     const nome = inputNome.value
     const telefone = inputTelefone.value
     const mensagem = textareaMensagem.value
+    const recrutador = recrutadorNome.value
+    const recrutadorID = recrutadorId.value
 
     const pontos = calcularPontuacao()
-    const resultado = pontos >= 11 ? '✅ Passou' : '❌ Reprovado'
+    const resultado = pontos >= 11 ? ' Passou ✅' : ' Reprovado ❌'
 
-    const dados = prepararDados(nome, telefone, mensagem, pontos, resultado)
+    const dados = prepararDados(nome, telefone, mensagem, pontos, resultado, recrutador, recrutadorID, link)
     enviarParaDiscord(dados, resultado)
 }
 
@@ -39,25 +44,24 @@ function calcularPontuacao () {
     return pontos
 }
 
-function prepararDados (nome, telefone, mensagem, pontos, resultado) {
+function prepararDados (nome, telefone, mensagem, pontos, resultado, recrutador, recrutadorID, link) {
     const dados = {
         content: `
+
 :green_circle: **Recrutamento resultado**
 
-:person_bald: **QRA**
-${nome}
+:military_helmet: **QRA:** ${nome} | ${telefone}
 
-:telephone: **ID**
-${telefone}
+:clipboard: **Pontuação:**${pontos} pontos
 
-:clipboard: **Pontuação**
-${pontos} pontos
+:page_facing_up: **Resultado:** ${resultado}
 
-:page_facing_up: **Resultado**
-${resultado}
+:envelope: **Comentário:** ${mensagem}
 
-:envelope: **Comentário**
-${mensagem}
+:identification_card: **Recrutador:** ${recrutador} #${recrutadorID}
+
+${link} :point_left: ***Envie outro aqui***
+
         `
     }
 
